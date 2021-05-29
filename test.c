8381cafe7a37e7092943aacd3c474fc5f61c6d42
Venugopalan <iyengarvenugopalan@gmail.com>
2021-05-29 15:22:05 +0530

    New File Created

---
+++ test.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  29/05/21 03:20:05 PM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main(){
+    return 0;
+}
